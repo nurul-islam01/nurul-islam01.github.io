ed37598990509af0978fd7207382062bb4869c81
nurul-islam-prothomalo <nurul.islam@prothomalo.com>
2023-12-26 00:50:15 +0600

    remove 118b

--- next.config.js
+++ next.config.js
@@ -25,10 +25,6 @@ const nextConfig = {
       '/contact': { page: '/contact' }
     };
   },
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en'
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
