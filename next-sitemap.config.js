4f04252107fd4768b0318db29f42afc67cfef07a
nurul-islam-prothomalo <nurul.islam@prothomalo.com>
2023-12-25 21:26:07 +0600

    updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.nurulislam.dev',
-  generateRobotsTxt: true // (optional)
-  // ...other options
+  generateRobotsTxt: true
 };
--- next.config.js
+++ next.config.js
@@ -1,25 +1,36 @@
 const path = require('path');
 const withOffline = require('next-offline');
+const withPwa = require('next-pwa');
+const withPlugin = require('next-compose-plugins');
 
-const nextConfig = {
-  reactStrictMode: true,
-  generateSw: true,
-  images: {
-    unoptimized: true
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack']
-    });
+const nextConfig = withPlugin(
+  [
+    withPwa,
+    {
+      generateSw: true,
+      workerName: 'service-worker.js'
+    },
+    withOffline
+  ],
+  {
+    reactStrictMode: true,
+    images: {
+      unoptimized: true
+    },
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')]
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack']
+      });
 
-    return config;
-  },
-  output: 'export'
-};
+      return config;
+    },
+    output: 'export'
+  }
+);
 
-module.exports = withOffline(nextConfig);
+module.exports = nextConfig;
