0ae15f1acea2c5560684876fa0016d47ea8aee90
nurul-islam-prothomalo <nurul.islam@prothomalo.com>
2023-12-26 00:35:44 +0600

    updated config

--- next.config.js
+++ next.config.js
@@ -1,46 +1,44 @@
 const path = require('path');
-const withOffline = require('next-offline');
-const withPwa = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
-const withPlugin = require('next-compose-plugins');
 
-const nextConfig = withPlugin(
-  [
-    [
-      withPwa,
-      {
-        pwa: {
-          dest: 'public',
-          runtimeCaching
-        }
-      }
-    ],
-    [
-      withOffline,
-      {
-        scope: '/'
-      }
-    ]
-  ],
-  {
-    reactStrictMode: true,
-    images: {
-      unoptimized: true
-    },
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')]
-    },
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack']
-      });
+const isProd = process.env.NODE_ENV === 'production';
 
-      return config;
-    },
-    output: 'export'
+const nextConfig = {
+  pwa: {
+    dest: 'public',
+    runtimeCaching
+  },
+  distDir: 'out',
+  scope: '/',
+  assetPrefix: isProd ? 'https://www.nurulislam.dev' : undefined,
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  eslint: { ignoreDuringBuilds: true },
+  exportPathMap: async () => {
+    return {
+      '/': { page: '/' },
+      '/resume': { page: '/resume' },
+      '/contact': { page: '/contact' }
+    };
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en'
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    });
+
+    return config;
   }
-);
+};
 
 module.exports = nextConfig;
