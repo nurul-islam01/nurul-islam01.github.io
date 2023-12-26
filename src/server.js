1597c69aedb823c0c99ef464474edc5ba3a6669f
nurul-islam-prothomalo <nurul.islam@prothomalo.com>
2023-12-26 23:47:32 +0600

    hy

--- src/server.js
+++ src/server.js
@@ -45,6 +45,6 @@ app.prepare().then(() => {
       process.exit(1);
     })
     .listen(port, () => {
-      console.log(`> Ready on https://${hostname}:${port}`);
+      console.log(`> Ready on http://${hostname}:${port}`);
     });
 });
--- src/services/mail.js
+++ src/services/mail.js
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   secure: true,
   port: 465,
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD
+    user: process.env.EMAIL || 'hello@nurulislam.dev',
+    pass: process.env.PASSWORD || 'sultan@20'
   }
 });
 
@@ -15,31 +15,9 @@ exports.sendMail = (body) => {
   const { subject = '', email, name = '', message = '' } = body;
 
   return transporter.sendMail({
-    from: email,
-    to: 'nurul.islam@nurulislam.dev, nurul.islam3f@gmail.com',
-    subject: subject,
-    text: message,
-    html: `<!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Email from ${email}</title>
-            </head>
-            <body style="font-family: Arial, sans-serif;">
-                <div style="max-width: 600px; margin: 0 auto; padding: 20px;">
-                    <h1>${subject}</h1>
-                    <p>Hello,</p>
-            
-                    <p>${message}</p>
-            
-                    <p>Regards,<br>
-                    Your Name</p>
-            
-                </div>
-            
-            </body>
-            </html>`
+    from: process.env.EMAIL || 'hello@nurulislam.dev',
+    to: 'nurul.islam@nurulislam.dev,nurul.islam3f@gmail.com',
+    subject: `${subject} from ${email}`,
+    text: message
   });
 };
