4456f53ed358a22e8a10836a33314c29489a5f7c
nurul-islam-prothomalo <nurul.islam@prothomalo.com>
2023-12-26 09:09:42 +0600

    remove service worker register

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import Container from '@/components/layouts/Container';
 
@@ -6,21 +6,6 @@ import '@/styles/globals.css';
 import '@/styles/app.css';
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function () {
-        navigator.serviceWorker
-          .register('/static/service-worker.js')
-          .then(function (registration) {
-            console.log('SW registered: ');
-          })
-          .catch(function (registrationError) {
-            console.log('SW registration failed: ', registrationError);
-          });
-      });
-    }
-  }, []);
-
   return (
     <Container>
       <Component {...pageProps} />
