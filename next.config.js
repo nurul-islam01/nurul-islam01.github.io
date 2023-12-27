6a89bce5c93d8c82c94fb05ccc9b05371bc77385
Nurul Islam <34369548+nurul-islam01@users.noreply.github.com>
2023-12-27 09:23:44 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,6 +15,7 @@ const withPWA = require('@ducanh2912/next-pwa').default({
 });
 
 const nextConfig = {
+  exportTrailingSlash: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   },
